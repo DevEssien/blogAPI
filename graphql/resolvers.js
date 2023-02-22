204d27904ee1294ff564d3ba389a130c9629f7cf
Essien Emmanuel <essienemma300@gmail.com>
2023-02-22 22:00:02 +0100

    added a createUser schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,30 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 module.exports = {
-    hello() {
+    createUser: async function ({ userInput }, req) {
+        const { email, name, password } = userInput;
+        const existingEmail = await User.findOne({ email: email });
+        if (existingEmail) {
+            const error = new Error("User with email address already exist");
+            error.statusCode = 404;
+            throw error;
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            email: email,
+            name: name,
+            password: hashedPassword,
+        });
+        const createdUser = await newUser.save();
+        if (!createdUser) {
+            const error = new Error("Server side failed");
+            error.statusCode = 500;
+            throw error;
+        }
         return {
-            text: "hello world!",
-            views: 123,
+            ...createdUser._doc,
+            _id: createdUser._id.toString(),
         };
     },
 };
